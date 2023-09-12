98aa086470f82ced147e3b88864422deab19e114
Tanveer J <mjan2021@fau.edu>
2023-09-12 16:41:34 -0400

    label and images

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ import cv2
 video_path = 'data/sample_videos/test.mp4'
 detector = ASOne(detector=asone.YOLOV7_PYTORCH, use_cuda=True) # Set use_cuda to False for cpu
 
-filter_classes = ['car'] # Set to None to detect all classes
+filter_classes = None # Set to None to detect all classes
 
 cap = cv2.VideoCapture(video_path)
 
@@ -14,6 +14,8 @@ file = open("./yolov8_annotations", "a+")
 	
 total_frames =  int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 counter += 0
+os.mkdir('./images')
+os.mkdir('./labels')
 while True:
 	_, frame = cap.read()
     	if not _:
@@ -27,10 +29,15 @@ while True:
 	bbox_xyxy = dets[:, :4]
     	scores = dets[:, 4]
     	class_ids = dets[:, 5]
-
-	for idx in range(0, len(bbox_xyxy)):
-		objects = f"{class_ids[idx]} {bbox_xyxy[0]} {bbox_xyxy[1]} {bbox_xyxy[2]} {bbox_xyxy[3]}"
-		file.write(str(objects))
+	
+	# saving images
+	cv.imwrite(f'./image/{counter}.jpg')
+	
+	# saving labels file
+	with open(f'./labels/{counter}.txt', 'w+') as file:
+		for idx in range(0, len(bbox_xyxy)):
+			objects = f"{class_ids[idx]} {bbox_xyxy[0]} {bbox_xyxy[1]} {bbox_xyxy[2]} {bbox_xyxy[3]}"
+			file.write(str(objects))
 
     	#frame = utils.draw_boxes(frame, bbox_xyxy, class_ids=class_ids)
 
